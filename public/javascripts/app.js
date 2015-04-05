bae6c77226b0684972b7472082ae6b2fe4d08208
chenlizhan <chung1350@hotmail.com>
2015-04-05 15:25:34 +0800

    Modify the queryed api into constant var

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,15 +1,8 @@
 var postsApp = angular.module('postsApp', []);
-
-// postsApp.config(function config($stateProvider) {
-//   $stateProvider.state("reddit", {
-//     url: "/reddit",
-//     controller: "RedditPostsController",
-//     templateUrl: "views/reddit-post.ejs"
-//   })
-// })
+var REDDIT_API_ALL_NEW = 'http://www.reddit.com/r/all/new.json'
 
 postsApp.controller('RedditPostsController', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://www.reddit.com/r/all/new.json')
+  $http.get(REDDIT_API_ALL_NEW)
     .success(function(data) {
       $scope.posts = data['data']['children'];
       $scope.test = 'test string';
